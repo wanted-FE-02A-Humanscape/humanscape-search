ac9e021c55d04c91e1e932ca025df6c050a67031
hyemin065 <p62517@gmail.com>
2022-05-21 23:43:17 +0900

    fix: fix css

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,20 +8,18 @@ import styles from './Routes.module.scss'
 
 export default function App() {
   return (
-    <>
-      <GNB />
-      <div className={styles.app}>
-        <ErrorBoundary FallbackComponent={Error}>
-          <header>
-            <div className={styles.title}>
-              <h1>국내 모든 임상시험 검색하고 온라인으로 참여하기</h1>
-            </div>
-          </header>
-          <main>
-            <Search />
-          </main>
-        </ErrorBoundary>
-      </div>
-    </>
+    <div className={styles.app}>
+      <ErrorBoundary FallbackComponent={Error}>
+        <header>
+          <GNB />
+          <div className={styles.title}>
+            <h1>국내 모든 임상시험 검색하고 온라인으로 참여하기</h1>
+          </div>
+        </header>
+        <main>
+          <Search />
+        </main>
+      </ErrorBoundary>
+    </div>
   )
 }
